86c57981533d1a471ded28b267d02e05253b22ed
midimurphdesigns <midimurphdesigns@gmail.com>
2018-06-13 07:54:02 -0700

    working on concert search page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -16,9 +16,9 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            {/* <h2>Welcome to Foo App</h2>
+            <h2>Welcome to Foo App</h2>
             <LoginForm />
-            <Link to="/register">Register</Link> */}
+            <Link to="/register">Register</Link>
         </div>
     );
 }
