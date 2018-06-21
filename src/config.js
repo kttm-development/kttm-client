b2b5218020356a694d63b6ebf06fcff579e9bb52
thaborak <thaborak@gmail.com>
2018-06-21 18:04:33 -0400

    https

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
     //process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
-    process.env.REACT_APP_API_BASE_URL || 'http://concertconnect-server.herokuapp.com/api';
+    process.env.REACT_APP_API_BASE_URL || 'https://concertconnect-server.herokuapp.com/api';
