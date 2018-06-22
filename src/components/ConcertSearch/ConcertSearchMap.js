1950a49212908d6a33571b787afc1503ec215c6b
trentduncan <trent.dunc@gmail.com>
2018-06-22 12:33:01 -0700

    center changing dynamically

--- src/components/ConcertSearch/ConcertSearchMap.js
+++ src/components/ConcertSearch/ConcertSearchMap.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 
 const ConcertSearchMap = withScriptjs(withGoogleMap((props) => {
     return (
         <GoogleMap
-            defaultZoom={6}
-            defaultCenter={props.mapCenter}
+            zoom={6}
+            center={props.mapCenter}
         >
             {props.concerts.map((item, index) => <Marker key={index} position={{ lat: item.coords.lat, lng: item.coords.lng }} />)}
         </GoogleMap>
     );
 }));
+
 
 
 
