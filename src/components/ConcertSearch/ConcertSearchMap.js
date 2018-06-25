16505487b843d651462b743e533d4e6b7e5cbd76
trentduncan <trent.dunc@gmail.com>
2018-06-25 09:46:33 -0700

    added logic to show infowindows for each marker

--- src/components/ConcertSearch/ConcertSearchMap.js
+++ src/components/ConcertSearch/ConcertSearchMap.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { compose, withProps, withStateHandlers } from 'recompose';
+import { Link } from 'react-router-dom'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import {storeCurrentConcert} from '../../actions/ticketmaster-actions'
 
 
-const ConcertSearchMap = withScriptjs(withGoogleMap((props) => {
-    return (
-        <GoogleMap
+const ConcertSearchMap = compose(
+    withStateHandlers(() => ({
+      isOpen: false,
+    }), {
+      onToggleOpen: ({ isOpen }) => () => ({
+        isOpen: !isOpen,
+      })
+    }),
+    withScriptjs,
+    withGoogleMap
+  )(props =>
+    <GoogleMap
             zoom={6}
             center={props.mapCenter}
         >
-            {props.concerts.map((item, index) => <Marker key={index} position={{ lat: item.coords.lat, lng: item.coords.lng }} />)}
+            {props.concerts.map((item, index) => {
+                const {city,
+                    state,
+                    date,
+                    id,
+                    name,
+                    image,
+                    time,
+                    venue,
+                    url,
+                    attraction,
+                    description} = item;
+                return (
+                <Marker key={index} position={{ lat: item.coords.lat, lng: item.coords.lng }} onClick={props.onToggleOpen}>
+                {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+                  <Link to='/concert-about' onClick={() => {
+                  const currentConcertObj = {
+                    city,
+                    state,
+                    date,
+                    id,
+                    name,
+                    image,
+                    time,
+                    venue,
+                    url,
+                    attraction,
+                    description
+                  }
+                  props.dispatch(storeCurrentConcert(currentConcertObj))
+                }}
+              >{item.name}</Link>
+                    </InfoWindow>}
+                </Marker>
+                )
+            })}
         </GoogleMap>
     );
-}));
 
 
 
--- src/components/ConcertSearch/ConcertSearchResults.js
+++ src/components/ConcertSearch/ConcertSearchResults.js
@@ -61,6 +61,8 @@ export class ConcertSearchResults extends React.Component {
                      isMarkerShown
                      mapCenter={this.props.mapCenter}
                      concerts={this.props.concerts}
+                     zoom={this.props.zoom}
+                     dispatch={this.props.dispatch}
                      googleMapURL={GOOGLE_MAPS_URL}
                      loadingElement={<div style={{ height: '100%' }} />}
                      containerElement={<div style={{ height: '300px' }} />}
