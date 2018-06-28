e9a80b0f132d0693a4f4f131cb33235c119ff347
Tom <thaborak@gmail.com>
2018-06-28 14:33:42 -0400

    lity npm uninstall

--- src/commons/YouTube.js
+++ src/commons/YouTube.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import lity from 'lity';
 import '../components/styles/lity.css'
 
 export class Youtube extends React.Component {
